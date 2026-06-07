e87d44f867d6f3f5780a1d74d750bab1cdbef14a
manvikjena14a-hub <manvikjena14a@gmail.com>
2026-06-07 14:14:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,75 @@
-function showSection(id){
-    document.querySelectorAll('.section').forEach(sec=>{
-        sec.style.display='none';
-    });
-    document.getElementById(id).style.display='block';
-}
-
-function askQuestion(){
-    let q=document.getElementById('question').value;
-    let ans="I am a demo AI assistant. You asked: " + q;
-    document.getElementById('answer').innerText=ans;
-}
-
-function checkAnswer(answer){
-    let result=document.getElementById('quizResult');
-
-    if(answer==="Water"){
-        result.innerText="✅ Correct!";
-    }else{
-        result.innerText="❌ Wrong!";
-    }
-}
-
-function saveNotes(){
-    localStorage.setItem(
-        "studyNotes",
-        document.getElementById("notesArea").value
-    );
-
-    alert("Notes Saved!");
-}
-
-window.onload=function(){
-    let notes=localStorage.getItem("studyNotes");
-    if(notes){
-        document.getElementById("notesArea").value=notes;
-    }
+{
+question:"What is the capital of India?",
+options:["Delhi","Mumbai","Kolkata"],
+answer:"Delhi"
+},
+{
+question:"Which planet is known as the Red Planet?",
+options:["Mars","Earth","Jupiter"],
+answer:"Mars"
+},
+{
+question:"What is 10 × 10 ?",
+options:["100","110","90"],
+answer:"100"
+},
+{
+question:"Which gas do plants absorb?",
+options:["Oxygen","Nitrogen","Carbon Dioxide"],
+answer:"Carbon Dioxide"
+},
+{
+question:"What is the chemical symbol of Sodium?",
+options:["Na","So","S"],
+answer:"Na"
+},
+{
+question:"Which organ pumps blood in the body?",
+options:["Brain","Heart","Lungs"],
+answer:"Heart"
+},
+{
+question:"How many continents are there?",
+options:["5","6","7"],
+answer:"7"
+},
+{
+question:"What is the largest planet?",
+options:["Earth","Jupiter","Mars"],
+answer:"Jupiter"
+},
+{
+question:"What is 15 + 25 ?",
+options:["35","40","45"],
+answer:"40"
+},
+{
+question:"Which vitamin is produced by sunlight?",
+options:["Vitamin A","Vitamin C","Vitamin D"],
+answer:"Vitamin D"
+},
+{
+question:"What is the boiling point of water?",
+options:["90°C","100°C","120°C"],
+answer:"100°C"
+},
+{
+question:"Who developed the theory of relativity?",
+options:["Newton","Einstein","Tesla"],
+answer:"Einstein"
+},
+{
+question:"What is the chemical formula of salt?",
+options:["NaCl","H2O","CO2"],
+answer:"NaCl"
+},
+{
+question:"Which planet has rings?",
+options:["Saturn","Mars","Mercury"],
+answer:"Saturn"
+},
+{
+question:"What is 9 × 8 ?",
+options:["72","81","64"],
+answer:"72"
 }
